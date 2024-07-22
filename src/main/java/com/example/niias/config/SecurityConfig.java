da22bddf1734ff0413940c21dee63a3e6db6bba9
dimapartov <mr_kubarev@mail.ru>
2024-07-22 16:42:57 +0300

    Fix SecurityConfig.java

--- src/main/java/com/example/niias/config/SecurityConfig.java
+++ src/main/java/com/example/niias/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     @Bean
-    public org.springframework.security.core.userdetails.UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService() {
         return new MyUserDetailsService();
     }
 
